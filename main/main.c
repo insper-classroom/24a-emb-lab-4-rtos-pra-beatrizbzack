fc88ff413708ed38d9fe8494798abe98dacddcda
beatrizbzack <beatrizbz@al.insper.edu.br>
2024-03-22 18:01:21 +0000

    configs

--- main/main.c
+++ main/main.c
@@ -17,14 +17,8 @@ const uint LED_2_OLED = 21;
 const uint LED_3_OLED = 22;
 
 // DISTANCIA E TIMER
-const int TRIG_PIN = 12;
-const int ECHO_PIN = 13;
-int SOUND_SPEED = 340;
-
-// volatile absolute_time_t start_time;
-// volatile absolute_time_t end_time;
-volatile uint64_t start_time;
-volatile uint64_t end_time;
+const int TRIG_PIN = 16;
+const int ECHO_PIN = 17;
 
 // criando uma fila
 QueueHandle_t xQueue_time;
@@ -63,17 +57,16 @@ void oled_task(void *p) {
     gfx_init(&disp, 128, 32);
     printf("Inicializando btn and LEDs\n");
     oled1_btn_led_init();
+    
     while (1) {
         float distance;
+        
         if ((xSemaphoreTake(xSemaphore_trigger, pdMS_TO_TICKS(100)) == pdTRUE)) {
             if (xQueueReceive(xQueue_distance, &distance, pdMS_TO_TICKS(50))) {
-                size_t tamanho_float = sizeof(float);
-                int tamanho_float_int = (int)tamanho_float;
-                char distance_str[tamanho_float_int+3];
+                char distance_str[20];
                 gfx_clear_buffer(&disp);
                 snprintf(distance_str, sizeof(distance_str), "%.2f", distance);
                 gfx_draw_string(&disp, 0, 0, 1, distance_str);
-                gfx_draw_line(&disp, 15, 27, distance, 27);
                 gfx_show(&disp);
                 vTaskDelay(pdMS_TO_TICKS(50));
             }
@@ -81,17 +74,18 @@ void oled_task(void *p) {
     }
 }
 
-
 // DISTANCIA E TIMER
 void pin_callback(uint gpio, uint32_t events) {
-    if (gpio == ECHO_PIN) {
-        if (gpio_get(ECHO_PIN)) {
-            // ECHO_PIN mudou para alto
+    static uint32_t start_time, end_time, duration;
+    
+    if (events == 0x8) {
+        if (gpio == ECHO_PIN) {
             start_time = to_us_since_boot(get_absolute_time());
-        } else {
-            // ECHO_PIN mudou para baixo
+        }
+    } else if (events == 0x4) {
+        if (gpio == ECHO_PIN) {
             end_time = to_us_since_boot(get_absolute_time());
-            uint64_t duration = end_time - start_time;
+            duration = end_time - start_time;
             xQueueSendFromISR(xQueue_time, &duration, 0);
         }
     }
@@ -114,6 +108,7 @@ void trigger_task() {
     gpio_init(TRIG_PIN);
     gpio_set_dir(TRIG_PIN, GPIO_OUT);
     gpio_put(TRIG_PIN, 0);
+    
     while (true) {
         send_pulse();
         xSemaphoreGive(xSemaphore_trigger);
@@ -132,6 +127,7 @@ void echo_sensor() {
     gpio_init(ECHO_PIN);
     gpio_set_dir(ECHO_PIN, GPIO_IN);
     gpio_pull_up(ECHO_PIN);
+    
     while (true) {
         if (xQueueReceive(xQueue_time, &duration, pdMS_TO_TICKS(50))) {
             float distance = duration / 58.0;
@@ -147,4 +143,17 @@ int main() {
     xQueue_time = xQueueCreate(32, sizeof(uint64_t)); // Ajuste conforme necessário
     xQueue_distance = xQueueCreate(32, sizeof(float)); // Ajuste conforme necessário
 
+    // TASKS E SCHEDULE
+    // task trigger:
+    xTaskCreate(trigger_task, "Trigger Sensor", 4095, NULL, 1, NULL);
+    // task echo:
+    xTaskCreate(echo_sensor, "Echo Sensor", 4095, NULL, 1, NULL);
+    // task oled:
+    xTaskCreate(oled_task, "OLED", 4095, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while (true) {
+        ;
+    }
 }
